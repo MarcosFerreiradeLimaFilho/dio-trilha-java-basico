81166c4984c1f25e9cebe3d65fbbb4bbdf48f693
MarcosFerreiradeLimaFilho <marcosferreiradelimafilho@gmail.com>
2022-10-08 16:02:09 -0300

    atualização

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -8,10 +8,33 @@ public class Operadores {
 
         System.out.println(variavel);
 
+        System.out.println("=================");
+        //Testando incrementação 
+
         int numero = 5;
 
         System.out.println(-- numero);
         System.out.println(++ numero);
 
+        System.out.println("==================");
+        //Testando operador ternário
+
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+        System.out.println("==================");
+        //Testando a usabilidade do .equals
+
+        String nomeUm = "Marcos";
+        String nomeDois = new String ("Marcos");
+
+        System.out.println(nomeUm == nomeDois);
+        System.out.println(nomeUm.equals(nomeDois));
     }
 }
