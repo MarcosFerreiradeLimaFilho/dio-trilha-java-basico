7da5742fe3323fbef282e153de1900075d3948e6
MarcosFerreiradeLimaFilho <marcosferreiradelimafilho@gmail.com>
2022-10-08 12:20:09 -0300

    teste de conversão.

--- tipos-variaveis/src/OperadoresLogicos.java
+++ tipos-variaveis/src/OperadoresLogicos.java
@@ -1,3 +1,11 @@
 public class OperadoresLogicos {
     
+    public static void main(String[] args) {
+        
+        boolean variavel = false;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+    }
 }
